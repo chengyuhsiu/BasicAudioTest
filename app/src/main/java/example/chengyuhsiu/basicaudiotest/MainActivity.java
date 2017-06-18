69aa7e319bb809d635e48b426c940567014e9306
chengyuhsiu <chengyuhsiu@gmail.com>
2017-06-18 16:46:59 +0800

    Add READ_EXTERNAL_STORAGE request

--- app/src/main/java/example/chengyuhsiu/basicaudiotest/MainActivity.java
+++ app/src/main/java/example/chengyuhsiu/basicaudiotest/MainActivity.java
@@ -1,10 +1,17 @@
 package example.chengyuhsiu.basicaudiotest;
 
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -39,6 +46,10 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnPre
 
     private Boolean mbIsInitialised = true;
 
+    private final int REQUEST_PERMISSION_FOR_READ_EXTERNAL_STORAGE = 100;
+
+    private String TAG="BasicAudioTest";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +63,25 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnPre
         mBtnMediaPlayPause.setOnClickListener(btnMediaPlayPauseOnClick);
         mBtnMediaPrev.setOnClickListener(btnMediaPrevOnClick);
         mBtnMediaNext.setOnClickListener(btnMediaNextOnClick);
+
+        Log.e(TAG, "before read external permission");
+        askForReadExternalStoragePermission();
+        Log.e(TAG, "after read external permission");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // 檢查收到的權限要求編號是否和我們送出的相同
+        if (requestCode == REQUEST_PERMISSION_FOR_READ_EXTERNAL_STORAGE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "取得 READ_EXTERNAL_STORAGE 權限", Toast.LENGTH_SHORT)
+                        .show();
+                return;
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
@@ -72,6 +102,8 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnPre
         mMediaPlayer.setOnPreparedListener(this);
         mMediaPlayer.setOnErrorListener(this);
         mMediaPlayer.setOnCompletionListener(this);
+
+
     }
 
     @Override
@@ -154,4 +186,49 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnPre
                 mMediaPlayer.setLooping(false);
         }
     };
+
+    private void askForReadExternalStoragePermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            // 這項功能尚未取得使用者的同意
+            // 開始執行徵詢使用者的流程
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    MainActivity.this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                AlertDialog.Builder altDlgBuilder =
+                        new AlertDialog.Builder(MainActivity.this);
+                altDlgBuilder.setTitle("提示");
+                altDlgBuilder.setMessage("App需要讀寫SD卡中的資料。");
+                altDlgBuilder.setIcon(android.R.drawable.ic_dialog_info);
+                altDlgBuilder.setCancelable(false);
+                altDlgBuilder.setPositiveButton("確定",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // 顯示詢問使用者是否同意功能權限的對話盒
+                                // 使用者答覆後會執行onRequestPermissionsResult()
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{
+                                                android.Manifest.permission.READ_EXTERNAL_STORAGE},
+                                        REQUEST_PERMISSION_FOR_READ_EXTERNAL_STORAGE);
+                            }
+                        });
+                altDlgBuilder.show();
+                Log.e(TAG, "askForReadExternal 1");
+                return;
+            } else {
+                // 顯示詢問使用者是否同意功能權限的對話盒
+                // 使用者答覆後會執行onRequestPermissionsResult()
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{
+                                android.Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_PERMISSION_FOR_READ_EXTERNAL_STORAGE);
+                Log.e(TAG, "askForReadExternal 2");
+                return;
+            }
+        } else {
+            Log.e(TAG, "Going wrong way");
+        }
+    }
 }
